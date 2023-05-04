fad090a45744917d5d707f7f3a9743a0e86ef106
andri <andriyparayko2017@gmail.com>
2023-05-04 00:02:54 -0700

    2.5 task

--- It in business/lab3.php
+++ It in business/lab3.php
@@ -1,17 +1,30 @@
 <?php
-echo("Приїхали");
+$fname = "не знайдено";
+$login = "не знайдено";
+$email = "не знайдено";
+$userPassword = "не знайдено";
+$userPassword2 = "не знайдено";
 
+if(isset($_POST["fname"])){
 
+    $fname = $_POST["fname"];
+}
 
+if(isset($_POST["login"])){
 
+    $login = $_POST["login"];
+}
 
+if(isset($_POST["email"])){
 
+    $email = $_POST["email"];
+}
 
+if(isset($_POST["userPassword"])){
 
-
-
-
-
+    $userPassword = $_POST["userPassword"];
+}
+echo "Ім'я: $fname <br> Логін: $login <br> E-mail: $email <br> Password: $userPassword <br>";
 
 
 
