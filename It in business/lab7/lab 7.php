8d38ebf2ef601ccfb8a03c1e7c5594ddde3b34b3
andri <andriyparayko2017@gmail.com>
2023-05-16 11:45:41 -0700

    6-9 task

--- It in business/lab7/lab 7.php
+++ It in business/lab7/lab 7.php
@@ -2,45 +2,48 @@
 echo("Task 6")."\n";
 
 class Person1{
+
     protected $name;
     protected $age;
     protected $occupation;
 
-    public function __construct(string $name, int $age, string $occupation)
-    {
-        $this->name = $name;
-        $this->age = $age;
-        $this->occupation = $occupation;
-
-        if ($this->name != $name)
-            throw new InvalidArgumentException(
-                "Name must be a string!"
-            );
-
-        if ($age <= 0)
-            throw new InvalidArgumentException(
-                "Age must be a non-negative integer!"
-            ); else return round($age);
-            
-
-//        if ($this->occupation != $occupation)
-//            throw new InvalidArgumentException(
-//                "Occupation must be a string!"
-//            );
+    public function __construct($name, $age, $occupation) {
+        $this->set_name($name);
+        $this->set_age($age);
+        $this->set_occupation($occupation);
     }
-    public function get_name()
-    {
+
+    public function get_name() {
         return $this->name;
     }
 
-    public function get_age()
-    {
+    public function get_age() {
         return $this->age;
     }
 
-    public function get_occupation()
-    {
+    public function get_occupation() {
         return $this->occupation;
+    }
+
+    public function set_name($name) {
+        if (!is_string($name)) {
+            throw new InvalidArgumentException('Name must be a string!');
+        }
+        $this->name = $name;
+    }
+
+    public function set_age($age) {
+        if (!is_integer($age) || $age < 0) {
+            throw new InvalidArgumentException('Age must be a non-negative integer!');
+        }
+        $this->age = $age;
+    }
+
+    public function set_occupation($occupation) {
+        if (!is_string($occupation)) {
+            throw new InvalidArgumentException('Occupation must be a string!');
+        }
+        $this->occupation = $occupation;
     }
 }
 
@@ -71,7 +74,7 @@ class Person2{
     }
 }
 
-class ComputerProgrammer extends Person{
+class ComputerProgrammer1 extends Person{
     public function introduce()
     {
         return parent::introduce() . " and I am a $this->occupation";
@@ -189,7 +192,57 @@ class Person3 implements CanGreet, CanSpeak, CanIntroduce{
 
 echo("Task 9")."\n";
 
+abstract class Person4{
+    public $name;
+    public $age;
+    public $gender;
 
+    public function __construct($name, $age, $gender){
+    $this->name = $name;
+    $this->age = $age;
+    $this->gender = $gender;
+    }
+
+    abstract public function introduce();
+    public function greet($name){
+        return "Hello $name";
+    }
+}
+
+final class Child extends Person4{
+    public $aspirations;
+
+    public function __construct($name, $age, $gender, $aspirations)
+    {
+        parent::__construct($name, $age, $gender);
+        $this->aspirations = $aspirations;
+    }
+    public function introduce(){
+        return "Hi, I'm $this->name and I am $this->age years old";
+    }
+    public function greet($name){
+        return "Hi $name, let's play!";
+    }
+    public function say_dreams(){
+        return "I would like to be a(n) " . say_list($this->aspirations) . " when I grow up.";
+    }
+}
+
+class ComputerProgrammer2 extends Person4{
+    public $occupation = "Computer Programmer";
+    public function introduce(){
+        return "Hello, my name is $this->name, I am $this->age years old and I am a(n) $this->occupation";
+    }
+
+    public function greet($name)
+    {
+        return parent::greet($name). ", I'm $this->name, nice to meet you";
+    }
+
+    public function advertise(){
+        return "Don't forget to check out my coding projects";
+    }
+}
 
 echo("Task 10")."\n";
 
